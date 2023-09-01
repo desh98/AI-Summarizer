7075a92c11d6aa7518e62588b680d68aa1f2e654
desh98 <73052432+desh98@users.noreply.github.com>
2023-09-02 00:30:46 +0530

    Imported API

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,23 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const options = {
+    method: 'GET',
+    url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
+    params: {
+      url: 'https://time.com/6266679/musk-ai-open-letter/',
+      length: '3'
+    },
+    headers: {
+      'X-RapidAPI-Key': '2f7a37451emsh7cb4c35b0bdb642p17e011jsncb1f513a117e',
+      'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com'
+    }
+};
+
 export const articleApi = createApi({
     reducePath: 'articleApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: ''https://article-extractor-and-summarizer.p.rapidapi.com/'
+    }),
     endpoints: (builder) => ({
         getSummary: builder.query({
             query: (params) => 'test'
